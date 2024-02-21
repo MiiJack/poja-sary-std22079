753487ef84e018a347db5d42e1df26df1bc0fce7
MiiJack <f.rama723@gmail.com>
2024-02-21 21:26:07 +0300

    feat: added the service

---
+++ src/main/java/school/hei/sary/service/ImageTransformationService.java
@@ -0,0 +1,44 @@
+package school.hei.sary.service;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import javax.imageio.ImageIO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import school.hei.sary.file.BucketComponent;
+import school.hei.sary.repository.ImageTransformationRepository;
+
+@Service
+public class ImageTransformationService {
+  private BucketComponent bucketComponent;
+  private ImageTransformationRepository imageTransformationRepository;
+
+  public String Grayscale(String id, MultipartFile imageToTransform) throws IOException {
+    // Read the image from the MultipartFile into a BufferedImage
+    BufferedImage img = ImageIO.read(imageToTransform.getInputStream());
+    int width = img.getWidth();
+    int height = img.getHeight();
+
+    // Convert the image to grayscale
+    BufferedImage grayscaleImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        int rgb = img.getRGB(i, j);
+        int r = (rgb >> 16) & 0xFF;
+        int g = (rgb >> 8) & 0xFF;
+        int b = rgb & 0xFF;
+        int gray = (r + g + b) / 3;
+        grayscaleImage.setRGB(i, j, (gray << 16) | (gray << 8) | gray);
+      }
+    }
+
+    File tempFile = File.createTempFile(id, ".jpg");
+    ImageIO.write(grayscaleImage, "jpg", tempFile);
+    String bucketKey = id + "_grayscale.jpg";
+    bucketComponent.upload(tempFile, bucketKey);
+    return bucketComponent.presign(bucketKey, Duration.ofMinutes(7)).toString();
+  }
+}
