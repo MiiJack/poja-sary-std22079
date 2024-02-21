9a5e91271492ca044549efad6881cdf5c8ac4ab0
MiiJack <f.rama723@gmail.com>
2024-02-21 23:04:32 +0300

    fix: fixed controller (again)

--- src/main/java/school/hei/sary/endpoint/rest/controller/ImageTransformationController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/ImageTransformationController.java
@@ -1,19 +1,21 @@
 package school.hei.sary.endpoint.rest.controller;
 
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import school.hei.sary.service.ImageTransformationService;
 
+@RestController
 public class ImageTransformationController {
-  ImageTransformationService imageTransformationService;
+  @Autowired private ImageTransformationService imageTransformationService;
 
   @RequestMapping(
       path = "/grayscale/{id}",
       method = RequestMethod.PUT,
       consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-  public String toGrayscale(@PathVariable(name= "id") String id, @RequestPart MultipartFile image)
+  public String toGrayscale(@PathVariable(name = "id") String id, @RequestPart MultipartFile image)
       throws IOException {
     return imageTransformationService.Grayscale(id, image);
   }
