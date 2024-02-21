070fcf5997df5869742c0e2c4d16264dfb4e2472
MiiJack <f.rama723@gmail.com>
2024-02-22 01:48:14 +0300

    chore: attempting to make the controller work

--- src/main/java/school/hei/sary/endpoint/rest/controller/ImageTransformationController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/ImageTransformationController.java
@@ -1,29 +1,89 @@
 package school.hei.sary.endpoint.rest.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Optional;
+import javax.imageio.ImageIO;
+import lombok.AllArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import school.hei.sary.file.BucketComponent;
 import school.hei.sary.service.ImageTransformationService;
 
 @RestController
+@AllArgsConstructor
 public class ImageTransformationController {
-  @Autowired private ImageTransformationService imageTransformationService;
+  private BucketComponent bucketComponent;
+  @Autowired private ImageTransformationService iTS;
+
+  public static final String directory = "image/";
 
   @RequestMapping(
       path = "/grayscale/{id}",
       method = RequestMethod.PUT,
       consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-  public String toGrayscale(@PathVariable(name = "id") String id, @RequestPart MultipartFile image)
+  public ResponseEntity<String> toGrayscale(
+      @PathVariable(name = "id") String id, @RequestPart(value = "file") MultipartFile image)
       throws IOException {
-    String grayscaled = imageTransformationService.Grayscale(id, image).toString();
-    System.out.println(grayscaled);
-    return null;
+    String fileSuffix = "." + FilenameUtils.getExtension(image.getOriginalFilename());
+    String filePrefix = id + "-original";
+    String transformedFilePrefix = id + "-grayscale";
+    String fileBucketKey = directory + filePrefix + fileSuffix;
+    String transformedFileBucketKey = directory + transformedFilePrefix + fileSuffix;
+
+    File fileToUpload = File.createTempFile(filePrefix, fileSuffix);
+    File tempFile = File.createTempFile(transformedFilePrefix, ".jpg");
+    ImageIO.write(iTS.Grayscale(id, image), "jpg", tempFile);
+
+    bucketComponent.upload(fileToUpload, fileBucketKey);
+    bucketComponent.upload(tempFile, transformedFileBucketKey);
+
+    return ResponseEntity.of(
+        Optional.of(
+            bucketComponent.presign(transformedFileBucketKey, Duration.ofMinutes(18)).toString()));
   }
 
   @GetMapping("/grayscale/{id}")
-  public String getGrayscaledImage(@PathVariable String id) {
-    return id + "is grayscaled";
+  public String getGrayscaleImage(@PathVariable String id) {
+    return id;
+  }
+
+  @RequestMapping(
+      path = "/resize/{id}",
+      method = RequestMethod.PUT,
+      consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+  public ResponseEntity<String> toResize(
+      @PathVariable(name = "id") String id,
+      @RequestPart(value = "file") MultipartFile image,
+      @RequestParam(value = "w") int width,
+      @RequestParam(value = "h") int height)
+      throws IOException {
+    String fileSuffix = "." + FilenameUtils.getExtension(image.getOriginalFilename());
+    String filePrefix = id + "-original";
+    String resizedFilePrefix = id + "-resize";
+    String fileBucketKey = directory + filePrefix + fileSuffix;
+    String resizedFileBucketKey = directory + resizedFilePrefix + fileSuffix;
+
+    File fileToUpload = File.createTempFile(filePrefix, fileSuffix);
+    File tempFile = File.createTempFile(resizedFilePrefix, ".jpg");
+    ImageIO.write(
+        iTS.Resize(id, ImageIO.read(image.getInputStream()), width, height), "jpg", tempFile);
+
+    bucketComponent.upload(fileToUpload, fileBucketKey);
+    bucketComponent.upload(tempFile, resizedFileBucketKey);
+
+    return ResponseEntity.of(
+        Optional.of(
+            bucketComponent.presign(resizedFileBucketKey, Duration.ofMinutes(18)).toString()));
+  }
+
+  @GetMapping("/resize/{id}")
+  public String getResizedImage(@PathVariable String id) {
+    return id;
   }
 }
--- src/main/java/school/hei/sary/service/ImageTransformationService.java
+++ src/main/java/school/hei/sary/service/ImageTransformationService.java
@@ -1,35 +1,22 @@
 package school.hei.sary.service;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
 import javax.imageio.ImageIO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import school.hei.sary.file.BucketComponent;
-import school.hei.sary.repository.ImageTransformationRepository;
 
 @Service
 public class ImageTransformationService {
-  private final BucketComponent bucketComponent;
-  private final ImageTransformationRepository imageTransformationRepository;
 
-  public ImageTransformationService(
-      BucketComponent bucketComponent,
-      ImageTransformationRepository imageTransformationRepository) {
-    this.bucketComponent = bucketComponent;
-    this.imageTransformationRepository = imageTransformationRepository;
-  }
-
-  private static final String directory = "image/";
-
-  public String Grayscale(String id, MultipartFile imageToTransform) throws IOException {
+  public BufferedImage Grayscale(String id, MultipartFile imageToTransform) throws IOException {
+    String transformedFilePrefix = id + "-grayscale";
     // Read the image from the MultipartFile into a BufferedImage
     BufferedImage img = ImageIO.read(imageToTransform.getInputStream());
     int width = img.getWidth();
     int height = img.getHeight();
-
     // Convert the image to grayscale
     BufferedImage grayscaleImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
     for (int i = 0; i < width; i++) {
@@ -43,10 +30,23 @@ public class ImageTransformationService {
       }
     }
 
-    File tempFile = File.createTempFile(id, "_grayscale");
+    File tempFile = File.createTempFile(transformedFilePrefix, ".jpg");
     ImageIO.write(grayscaleImage, "jpg", tempFile);
-    String bucketKey = directory + id + grayscaleImage.toString();
-    bucketComponent.upload(tempFile, bucketKey);
-    return bucketComponent.presign(bucketKey, Duration.ofMinutes(7)).toString();
+    return grayscaleImage;
+  }
+
+  public BufferedImage Resize(
+      String id, BufferedImage imageToTransform, int targetWidth, int targetHeight)
+      throws IOException {
+    String transformedFilePrefix = id + "-resize";
+    Image resultingImage =
+        imageToTransform.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH);
+    BufferedImage outputImage =
+        new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+    outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+
+    File tempFile = File.createTempFile(transformedFilePrefix, ".jpg");
+    ImageIO.write(outputImage, "jpg", tempFile);
+    return outputImage;
   }
 }
